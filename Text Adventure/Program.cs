6357d35051158fd04bcaa8c7adcc73f40d3b9303
MUKILTEO\1722758 <1722758@mukilteo.wednet.edu>
2024-10-03 10:01:32 -0700

    First Commit.

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hic Sunt Leones.");
+
+            string input = Console.ReadLine();
+
+            if (input == "HIC SUNT LEONES!")
+            {
+                Console.WriteLine("Break The Chains.");
+            }
+
+            else if (input == "Hic Sunt Leones.")
+            {
+                Console.WriteLine("Welcome H.");
+
+                    Console.WriteLine("further instruction?"); 
+
+                string input3 = Console.ReadLine();
+                if (input3 == "Continue");
+                {
+                    Console.WriteLine(); // HERE*
+                }
+
+
+            }
+
+            else
+
+            {
+                Console.WriteLine("Say Again?");
+            }
+            string input2 = Console.ReadLine();
+
+            if (input2 =="HIC SUNT LEONES!")
+            {
+                Console.WriteLine("Close Enough.");
+            }
+
+            else
+            {
+                Console.WriteLine("Excute This One.");
+            }
+
         }
     }
 }
